c3de77adacd99cbebfb1941d0f63e6d5b6ce8f00
Dmitro.savenko <90914753+idkwhatimdoing1496@users.noreply.github.com>
2021-12-28 00:27:50 +0200

    Delete js directory

--- js/preload.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    let preloader = document.getElementById('preloader');
-    preloader.classList.add('hide-preloader');
-    setInterval(function () {
-        preloader.classList.add('preloader-hidden');
-    }, 400);
-}
